12944948a6547043a9fa8f6cd415b4cac1d68cab
rohan <rohanmalikno1@gmail.com>
2023-07-18 21:28:51 +0530

    Nav Bar set

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// images
+import Logo from '../assets/logo.svg';
+
+const Header = () => {
+  return <div className='bg-pink-300'>Header</div>;
+};
+
+export default Header;
